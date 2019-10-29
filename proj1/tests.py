35c443ad47beeabb8f6a15766b51732e6ce6c346
Michal <michal.pestka@protonmail.com>
2019-10-29 22:29:14 +0100

    new case draft

--- proj1/tests.py
+++ proj1/tests.py
@@ -7,10 +7,21 @@ class Test_utility_functions(unittest.TestCase):
         self.assertTrue(False)
 
     def test_get_k(self):
-        
+
         self.assertTrue(False)
 
     def test_get_m(self):
+        self.assertTrue(False)
+
+class Test_solver(unittest.TestCase):
+
+    def simple_example(self):
+        self.assertTrue(False)
+
+    def large_example(self):
+        self.assertTrue(False)
+
+    def impossible_example(self):
         self.assertTrue(False)
 
 if(__name__ == '__main__'):
